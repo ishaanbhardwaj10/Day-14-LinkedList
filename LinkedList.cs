ee633124e10be15295ea344a66d110f63433799f
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-14 16:41:36 +0530

    added AddAppend() method for UC-3

--- LinkedList.cs
+++ LinkedList.cs
@@ -25,6 +25,25 @@ namespace LinkedListProblem
         }
 
 
+        internal void AddAppend(int data)
+        {
+            Node newNode = new Node(data);
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = newNode;
+            }
+        }
+
+
         internal void Display()
         {
             Node temp = head;
