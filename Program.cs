3a958a610b693da65299c06975033e142a206dbf
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-14 16:42:20 +0530

    added UC-3: append to linked list

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace LinkedListProblem
             Console.WriteLine("Welcome to Linked List Problem");
             Console.WriteLine("Use case #1: Create a Simple Linked List");
             Console.WriteLine("Use case #2: Add to LinkedList in reverse");
+            Console.WriteLine("Use case #3: Append to LinkedList");
 
             Console.Write("Please select a program to run from options above: ");
             int choice = Convert.ToInt32(Console.ReadLine());
@@ -31,6 +32,13 @@ namespace LinkedListProblem
                     listOne.AddReverseData(30);
                     listOne.AddReverseData(56);
                     listOne.Display();
+                    break;
+                case 3:
+                    LinkedList listTwo = new LinkedList();
+                    listTwo.AddAppend(56);
+                    listTwo.AddAppend(30);
+                    listTwo.AddAppend(70);
+                    listTwo.Display();
                     break;
                 default:
                     Console.WriteLine("Please enter a valid number from given options");
