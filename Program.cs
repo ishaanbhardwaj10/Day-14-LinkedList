e5e005787b7bab0a93f63401c7ba0a38fe2e3f57
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-14 16:45:50 +0530

    added UC-4: insert at particular position in linked list

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace LinkedListProblem
             Console.WriteLine("Use case #1: Create a Simple Linked List");
             Console.WriteLine("Use case #2: Add to LinkedList in reverse");
             Console.WriteLine("Use case #3: Append to LinkedList");
+            Console.WriteLine("Use case #4: Insert 30 between 56 and 70 in linkedlist");
 
             Console.Write("Please select a program to run from options above: ");
             int choice = Convert.ToInt32(Console.ReadLine());
@@ -39,6 +40,15 @@ namespace LinkedListProblem
                     listTwo.AddAppend(30);
                     listTwo.AddAppend(70);
                     listTwo.Display();
+                    break;
+                case 4:
+                    LinkedList listThree = new LinkedList();
+                    listThree.AddAppend(56);
+                    listThree.AddAppend(70);
+                    listThree.Display();
+                    listThree.Insert(2, 30);
+                    Console.WriteLine("----------After insert operation----------");
+                    listThree.Display();
                     break;
                 default:
                     Console.WriteLine("Please enter a valid number from given options");
