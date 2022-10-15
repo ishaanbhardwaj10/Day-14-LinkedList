45b71fea5345e241d5b15c30b017ea9323f39bc0
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-15 18:32:38 +0530

    added a new model NodeS class with generics

---
+++ NodeS.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblem
+{
+    internal class NodeS<T> : IComparable where T : IComparable
+    {
+
+        public T data;
+        public NodeS<T> next;
+
+        public NodeS(T data)
+        {
+            this.data = data;
+        }
+
+        public int CompareTo(Object obj)
+        {
+            NodeS<T> incomingNode = (NodeS<T>)obj;
+            return this.data.CompareTo(incomingNode.data);
+        }
+
+
+    }
+}
